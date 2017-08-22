2c9c316f7f36b48816114112aeb96a8d7001e89b
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 21:14:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing() {
     return `There is nobody waiting to be served!`
   }
   else {
-    return katzDeliLine[0];
+    return `Currently serving ${katzDeliLine[0].name}`
     katzDeliLine.slice(1);
   }
 }
