466445fa766495a044a7041f280899ddff27273e
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 21:23:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   katzDeliLine = katzDeli;
-  return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 function nowServing() {
