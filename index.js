9869958df044e6fa94a3a27d6ab40dcca521e20e
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 21:21:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing() {
   if(katzDeli.length > 0) {
-    return `Currently serving ${katzDeliLine.shift()}`
+    return `Currently serving ${katzDeli.shift()}`
   }
   else {
     return `There is nobody waiting to be served!`
